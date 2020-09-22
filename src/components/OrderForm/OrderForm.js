5f6e6e20d9a1c926773b2d93463d57237e0eaa26
Tyler Haglund <relyt4me@gmail.com>
2020-09-22 11:57:25 -0600

    Add indication of buttons being clicked

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './OrderForm.css';
 
 class OrderForm extends Component {
   constructor(props) {
@@ -16,7 +17,14 @@ class OrderForm extends Component {
     if (!this.state.ingredients.includes(e.target.name)) {
       this.setState({ ingredients: [...this.state.ingredients, e.target.name] });
     }
+    e.target.classList.add('on-order');
   };
+
+  removeColorsFromButtons() {
+    document.querySelectorAll('.ingredient-button').forEach((button) => {
+      button.classList.remove('on-order');
+    });
+  }
 
   handleNameChange = (e) => {
     this.setState({ name: e.target.value });
@@ -29,6 +37,7 @@ class OrderForm extends Component {
       this.setState({ invalidOrder: false });
       this.props.addNewOrder(name, ingredients);
       await this.clearInputs();
+      this.removeColorsFromButtons();
     } else {
       this.setState({ invalidOrder: true });
     }
@@ -42,7 +51,7 @@ class OrderForm extends Component {
     const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
     const ingredientButtons = possibleIngredients.map((ingredient) => {
       return (
-        <button key={ingredient} name={ingredient} onClick={(e) => this.handleIngredientChange(e)}>
+        <button className='ingredient-button' key={ingredient} name={ingredient} onClick={(e) => this.handleIngredientChange(e)}>
           {ingredient}
         </button>
       );
@@ -50,9 +59,8 @@ class OrderForm extends Component {
 
     return (
       <form>
-        <input type='text' placeholder='Name' name='name' value={this.state.name} onChange={(e) => this.handleNameChange(e)} />
-
-        {ingredientButtons}
+        <input className='name-input' type='text' placeholder='Name' name='name' value={this.state.name} onChange={(e) => this.handleNameChange(e)} />
+        <div className='buttons-wrapper'> {ingredientButtons}</div>
         {this.state.invalidOrder && <p>An order must contain ingredients and a name</p>}
         <p>Order: {this.state.ingredients.join(', ') || 'Nothing selected'}</p>
 
