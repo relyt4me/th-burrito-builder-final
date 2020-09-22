c9c26c184259714a7d4d1c85c932186704933ec4
Tyler Haglund <relyt4me@gmail.com>
2020-09-22 09:43:36 -0600

    Create addOrder api call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,16 @@
 export const getOrders = () => {
-  return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
-}
+  return fetch('http://localhost:3001/api/v1/orders').then((response) => response.json());
+};
+
+export const addOrder = (order) => {
+  const stringyOrder = JSON.stringify(order);
+  return fetch(`http://localhost:3001/api/v1/orders`, {
+    method: 'Post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: stringyOrder,
+  }).then((response) => {
+    return response.json();
+  });
+};
