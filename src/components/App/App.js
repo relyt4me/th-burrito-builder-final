4daaae4e54e810c2ea5f1231b022fd812064c758
Tyler Haglund <relyt4me@gmail.com>
2020-09-22 09:18:00 -0600

    Initial commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import { getOrders } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
   constructor(props) {
     super();
+    this.state = {
+      orders: [],
+    };
   }
 
   componentDidMount() {
     getOrders()
-      .catch(err => console.error('Error fetching:', err));
+      .then((orders) => {
+        this.setState({ orders: orders.orders });
+      })
+      .catch((err) => console.error('Error fetching:', err));
   }
 
   render() {
     return (
-      <main className="App">
+      <main className='App'>
         <header>
           <h1>Burrito Builder</h1>
           <OrderForm />
         </header>
 
-        <Orders orders={this.state.orders}/>
+        <Orders orders={this.state.orders} />
       </main>
     );
   }
 }
-
 
 export default App;
