3fd41a496520d1ca01365bbeceea01c5bb1687d1
Tyler Haglund <relyt4me@gmail.com>
2020-09-22 09:30:58 -0600

    Make form controlled

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -6,49 +6,49 @@ class OrderForm extends Component {
     this.props = props;
     this.state = {
       name: '',
-      ingredients: []
+      ingredients: [],
     };
   }
 
+  handleIngredientChange = (e) => {
+    e.preventDefault();
+    this.setState({ ingredients: [...this.state.ingredients, e.target.name] });
+  };
 
-  handleSubmit = e => {
+  handleNameChange = (e) => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleSubmit = (e) => {
     e.preventDefault();
     this.clearInputs();
-  }
+  };
 
   clearInputs = () => {
-    this.setState({name: '', ingredients: []});
-  }
+    this.setState({ name: '', ingredients: [] });
+  };
 
   render() {
     const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
-    const ingredientButtons = possibleIngredients.map(ingredient => {
+    const ingredientButtons = possibleIngredients.map((ingredient) => {
       return (
-        <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
+        <button key={ingredient} name={ingredient} onClick={(e) => this.handleIngredientChange(e)}>
           {ingredient}
         </button>
-      )
+      );
     });
 
     return (
       <form>
-        <input
-          type='text'
-          placeholder='Name'
-          name='name'
-          value={this.state.name}
-          onChange={e => this.handleNameChange(e)}
-        />
+        <input type='text' placeholder='Name' name='name' value={this.state.name} onChange={(e) => this.handleNameChange(e)} />
 
-        { ingredientButtons }
+        {ingredientButtons}
 
-        <p>Order: { this.state.ingredients.join(', ') || 'Nothing selected' }</p>
+        <p>Order: {this.state.ingredients.join(', ') || 'Nothing selected'}</p>
 
-        <button onClick={e => this.handleSubmit(e)}>
-          Submit Order
-        </button>
+        <button onClick={(e) => this.handleSubmit(e)}>Submit Order</button>
       </form>
-    )
+    );
   }
 }
 
